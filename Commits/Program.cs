02b451a962034afacc09954dd9e68ba2540b1dfa
JEANNA KAWAMOTO <kawzie30@live.com>
2018-07-16 15:26:03 -0700

    Made a GitHub write line statement reminder to fetch and pull changes

--- Commits/Program.cs
+++ Commits/Program.cs
@@ -11,6 +11,7 @@ namespace Commits
         static void Main(string[] args)
         {
             Console.WriteLine("I think I successfully created a repository for 4th attempt. This statement is my first commit for this repository.");
+            Console.WriteLine("Get in the habit of fetching code from any changes on GitHub,  then pull to retrieve the changees. This statement is my second commit" )
             Console.ReadKey();
         }
     }
