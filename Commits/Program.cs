203f96f52b7a192cc91457061af6c242aa791230
JEANNA KAWAMOTO <kawzie30@live.com>
2018-07-16 15:21:34 -0700

    One statement added as my first commit

--- Commits/Program.cs
+++ Commits/Program.cs
@@ -10,6 +10,8 @@ namespace Commits
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("I think I successfully created a repository for 4th attempt. This statement is my first commit for this repository.");
+            Console.ReadKey();
         }
     }
 }
