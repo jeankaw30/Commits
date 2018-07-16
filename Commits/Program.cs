e5cfe1c247ec86fe922baca7fb237ee4cf6ad8a5
JEANNA KAWAMOTO <kawzie30@live.com>
2018-07-16 15:29:43 -0700

    Made another note about pushing to GitHub commits. This is my third commit

--- Commits/Program.cs
+++ Commits/Program.cs
@@ -11,7 +11,8 @@ namespace Commits
         static void Main(string[] args)
         {
             Console.WriteLine("I think I successfully created a repository for 4th attempt. This statement is my first commit for this repository.");
-            Console.WriteLine("Get in the habit of fetching code from any changes on GitHub,  then pull to retrieve the changees. This statement is my second commit" )
+            Console.WriteLine("Note : Get in the habit of fetching code from any changes on GitHub,  then pull to retrieve the changees. This statement is my second commit" )
+            Console.WriteLine("Note : After committing, anmd syncing in visual studio that you have to push in order for GitHub to retrieve online. This reminder is my third commit.");
             Console.ReadKey();
         }
     }
